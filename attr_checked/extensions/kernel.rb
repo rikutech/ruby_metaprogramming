ca2d73055a48529916b1c80e54a90a754397990c
rikutech <rikutech@gmail.com>
2018-04-07 20:50:03 +0900

    initial commit

---
+++ attr_checked/extensions/kernel.rb
@@ -0,0 +1,5 @@
+module Kernel
+  def add_checked_attribute(attr)
+
+  end
+end
---
+++ attr_checked/main.rb
@@ -0,0 +1,4 @@
+  /Users/Rikuto/src/ruby/metaprg:
+  total used in directory 0 available 4292789513
+  drwxr-xr-x  2 Rikuto  staff   68  4  7 19:29 .
+  drwxr-xr-x  5 Rikuto  staff  170  2  5 10:53 ..
